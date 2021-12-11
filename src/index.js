845734c2aeb6c0fcc656227ab2264be18440fdcb
Trevor Richardson <trevor@gaincompliance.com>
2021-12-10 20:38:55 -0600

    feat(server): add ws event handling to the http express server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
 const app = express();
 const PORT = process.env.PORT || 8000
 
-app.get('/', (_, res) => res.send('hello express!'));
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ clientTracking: false, noServer: true });
 
-app.listen(PORT, () => {
-    console.log('launching the express server!');
+server.on('upgrade', function (request, socket, head) {
+    console.log('handling upgrade from http server. attempting websocket connection...');
+  
+    wss.handleUpgrade(request, socket, head, function (ws) {
+      wss.emit('connection', ws, request);
+    });
+  });
+
+  wss.on("connection", function (ws, request) {
+    ws.on("message", function (message) {
+      console.log(`Received message ${message}`);
+    });
+
+    ws.on("close", function () {
+      console.log("closing socket connection...");
+    });
+  });
+  
+app.get('/ws', (_, res) => res.send('this is a websocket endpoint. ask for an upgrade and you will get it!'));
+
+server.listen(PORT, function () {
+  console.log(`Listening on http://localhost:${PORT}`);
 });
